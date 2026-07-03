b109033fee353750dd57ba117dee0ac58ed6bbae
junairjnr <junairjunu869@gmail.com>
2026-07-03 11:43:24 +0530

    render url added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://azm-b.onrender.com/api';
 
 class ApiError extends Error {
   status: number;
